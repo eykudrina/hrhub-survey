b447477c9f23197b442ab5f44b584a1ca18b86ac
eykudrina <eykudrina@gmail.com>
2026-05-22 16:26:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def analyze():
     return jsonify(result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
